9609b00807dce40d2b8c3ac9bb77c9d25205ed68
adina778899 <adinamemetea@yahoo.com>
2025-01-24 16:44:16 -0600

    Initial Commit

---
+++ ELVIS/ELVISApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELVISApp.swift
+//  ELVIS
+//
+//  Created by natural health on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ELVISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
